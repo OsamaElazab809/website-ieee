eefd03a24d2a6a052e5629ea11cc7821195a6d09
osama elazab <osamaelazab108@gmail.com>
2024-03-06 11:04:10 -0800

    responsive

--- js/main.js
+++ js/main.js
@@ -1,64 +1,98 @@
-// start header 
-const header=document.querySelector(".header");
-window.onscroll=function (){
-    if(scrollY>50){
-    header.style.cssText="background-color: var(--second-color);"
+// start header
+const header = document.querySelector(".header");
+window.onscroll = function () {
+    if (scrollY > 50) {
+        header.style.cssText = "background-color: var(--second-color);";
+    } else {
+        header.style.cssText = "background-color: transparent;";
     }
-    else{
-        header.style.cssText="background-color: transparent;"
+};
+
+const dropDown = document.querySelector(".header .dropdown");
+const linkDrop = document.querySelector(".header .dropdown .inner-links");
+dropDown.addEventListener("click", () => {
+    if (linkDrop.style.display === "block") {
+        linkDrop.style.display = "none";
+    } else {
+        linkDrop.style.display = "block";
     }
+});
+
+const links = document.querySelector(".header .links");
+const bars = document.querySelector(".header .bars-icons");
+const firstSpan = document.querySelector(
+    ".header .bars-icons span:first-child"
+);
+const midelSpan = document.querySelector(
+    ".header .bars-icons span:nth-child(2)"
+);
+const lastSpan = document.querySelector(".header .bars-icons span:last-child");
+bars.addEventListener("click", () => {
+    if (links.style.display === "none") {
+        links.style.display = "flex";
+        firstSpan.style.cssText = " margin-top: 9px;transform: rotate(45deg);";
+        midelSpan.style.opacity = "0";
+        lastSpan.style.cssText = "margin-top: 9px;transform: rotate(-45deg);";
+    } else {
+        links.style.cssText = "display:none;@media(max-width:992px){display:flex}";
+        firstSpan.style.cssText = " margin-top: 0px;transform: rotate(0deg);";
+        midelSpan.style.opacity = "1";
+        lastSpan.style.cssText = "margin-top: 17px;transform: rotate(0deg);";
+    }
+});
+// End header
+
+// Create a match Function
+
+function myFunction(x) {
+    if (x.matches) {
+        links.style.display = "flex";
+    } 
 }
 
-const dropDown=document.querySelector(".header .dropdown");
-const linkDrop=document.querySelector(".header .dropdown .inner-links");
-dropDown.addEventListener("click",()=>{
-    if( linkDrop.style.display==="block"){
-    linkDrop.style.display="none";
-    }else{
-        linkDrop.style.display="block";
-    }
-})
+const mmObj = window.matchMedia("(min-width: 991px)");
 
-// End header 
+mmObj.addEventListener("change", function () {
+    myFunction(mmObj);
+});
 
-// start Landing 
+// start Landing
 
-const landText=document.querySelector(".landing .content");
-landText.style.cssText="width:100%;text-align:center;"
+const landText = document.querySelector(".landing .content");
+landText.style.cssText = "width:100%;text-align:center;";
 
-const join=document.querySelector('.header .btn');
-join.addEventListener("click",()=>{
-    logregBox.style.transform="translateX(0%)";
-    landText.style.cssText="width:60%;text-align:left;"
-})
+const logregBox = document.querySelector(".logReg-box");
+const registerLink = document.querySelector(".login-link");
+const loginLink = document.querySelector(".register-link");
+const login = document.querySelector(".landing .form-box.login");
+const register = document.querySelector(".landing .form-box.register");
 
+const join = document.querySelector(".header .btn");
+join.addEventListener("click", () => {
+    login.style.cssText = "transform:translatex(-512px);transition:3s";
+    landText.style.cssText = "width:60%;text-align:left;";
+});
+registerLink.addEventListener("click", () => {
+    //     // logregBox.classList.add("active");
+    register.style.cssText = "transform: translateX(512px);transition:3s;";
+    login.style.cssText =
+        "transform: translateX(-512px);transition:3s;transition-delay:1s;";
+});
 
-const logregBox=document.querySelector(".logReg-box");
-const registerLink=document.querySelector(".login-link");
-const loginLink=document.querySelector(".register-link");
-const login=document.querySelector(".landing .form-box.login");
-const register=document.querySelector(".landing .form-box.register");
-
-registerLink.addEventListener("click",()=>{
+loginLink.addEventListener("click", () => {
     // logregBox.classList.add("active");
-    register.style.cssText="transform: translateX(450px);transition:3s;";
-    login.style.cssText="transform: translateX(0px);transition-delay:4s);";
-})
+    login.style.cssText = "transform: translateX(512px);transition:3s;";
+    register.style.cssText =
+        "transform: translateX(-512px);transition:3s;transition-delay:0.9s;";
+});
+// End Landing
 
-loginLink.addEventListener("click",()=>{
-    // logregBox.classList.add("active");
-    login.style.cssText="transform: translateX(450px);transition:3s;";
-    register.style.cssText="transform: translateX(0px);transition-delay:4s);";
-})
-// End Landing 
-
-// start board 
+// start board
 // const contBoard=document.querySelector(".board .container");
 // console.log(contBoard);
 // const more=document.querySelector(".board .board-button");
 // console.log(more);
 // const activeItem=document.querySelectorAll(".item");
-
 
 // activeItem.classList.add("active");
 // console.log(document.querySelector(".item.active"));
@@ -70,7 +104,7 @@ loginLink.addEventListener("click",()=>{
 // })
 // console.log(document.querySelector(".item.active"));
 
-// End board 
+// End board
 // const moreDetail=document.querySelectorAll(".Committees .btn a");
 // const showP=document.querySelectorAll(".Committees .commit-details p");
 // console.log(moreDetail)
